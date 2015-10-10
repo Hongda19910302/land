4604ddf330d52acdea2a8994e4185a8262848759
syacht <lisq037@163.com>
2015-10-10 18:11:21 +0800

    服务层常量

---
+++ src/main/java/net/deniro/land/common/service/Constants.java
@@ -0,0 +1,22 @@
+package net.deniro.land.common.service;
+
+/**
+ * 常量
+ *
+ * @author deniro
+ *         15-4-1上午10:29
+ */
+public class Constants {
+    /**
+     * Java 换行符
+     */
+    public static final String NEW_LINE = System.getProperty("line.separator");
+    /**
+     * js 换行符
+     */
+    public static final String JS_NEW_LINE = "\n";
+    /**
+     * html 换行符
+     */
+    public static final String HTML_NEW_LINE = "<br>";
+}
