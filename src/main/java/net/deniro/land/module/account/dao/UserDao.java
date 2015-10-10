6256223c2d949fb99e82d0b618de8c59a2151919
syacht <lisq037@163.com>
2015-10-10 17:06:18 +0800

    依据账号获取用户信息

---
+++ src/main/java/net/deniro/land/module/account/dao/UserDao.java
@@ -0,0 +1,28 @@
+package net.deniro.land.module.account.dao;
+
+import net.deniro.land.common.dao.BaseDao;
+import net.deniro.land.module.account.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 用户
+ *
+ * @author deniro
+ *         2015/10/10
+ */
+@Repository
+public class UserDao extends BaseDao<User> {
+
+    /**
+     * 依据账号获取用户信息
+     *
+     * @param account 账号
+     * @return
+     */
+    public List<User> findByAccount(String account) {
+        String hql = " from TUser where status=0 and account = ?";
+        return this.find(hql, new Object[]{account});
+    }
+}
