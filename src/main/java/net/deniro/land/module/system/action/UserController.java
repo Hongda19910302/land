8a5d62ea0a850473b6e15218ec9a56802c498414
syacht <lisq037@163.com>
2015-10-13 10:58:27 +0800

    当前登录账号写入session

--- src/main/java/net/deniro/land/module/system/action/UserController.java
+++ src/main/java/net/deniro/land/module/system/action/UserController.java
@@ -4,11 +4,14 @@ import net.deniro.land.common.service.dwz.Result;
 import net.deniro.land.common.service.dwz.ResultError;
 import net.deniro.land.module.system.service.UserService;
 import nl.captcha.Captcha;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,6 +24,8 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    static Logger logger = Logger.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -38,9 +43,6 @@ public class UserController {
     }
 
 
-
-
-
     /**
      * 登录
      *
@@ -56,18 +58,28 @@ public class UserController {
     public Result login(String account, String password, String code, int
             loginSourceCode, HttpSession session) {
 
-        //判断验证码是否正确
-        Captcha captcha = (Captcha) session.getAttribute(Captcha.NAME);
-        if (!captcha.isCorrect(code)) {
-            return new ResultError("验证码不正确！");
+        try {
+            //判断验证码是否正确
+            Captcha captcha = (Captcha) session.getAttribute(Captcha.NAME);
+            if (!captcha.isCorrect(code)) {
+                return new ResultError("验证码不正确！");
+            }
+
+            Result result = userService.login(account, password, loginSourceCode);
+
+            {//当前登录账号写入session
+                if (result.isSuccess())
+                    session.setAttribute(UserService.USER_CODE, result.get(UserService
+                            .USER_CODE));
+                if (session.getAttribute(UserService.USER_CODE) == null) {
+                    logger.error("写入session不成功！");
+                }
+            }
+
+            return result;
+        } catch (Exception e) {
+            logger.error("登录", e);
+            return new ResultError();
         }
-
-        Result result = userService.login(account, password, loginSourceCode);
-
-        if (result.isSuccess()) {//写入session
-            session.setAttribute(UserService.USER_CODE, result.get(UserService.USER_CODE));
-        }
-
-        return result;
     }
 }
