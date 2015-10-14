8faf39d73399e5c7f05bf467e61d57ab03880b0d
syacht <lisq037@163.com>
2015-10-14 18:31:45 +0800

    分页查询角色

--- src/main/java/net/deniro/land/module/system/action/RoleController.java
+++ src/main/java/net/deniro/land/module/system/action/RoleController.java
@@ -1,7 +1,12 @@
 package net.deniro.land.module.system.action;
 
+import net.deniro.land.common.dao.Page;
+import net.deniro.land.common.entity.QueryParam;
+import net.deniro.land.module.system.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 角色
@@ -13,6 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/role")
 public class RoleController {
 
+    @Autowired
+    private RoleService roleService;
+
+    /**
+     * 分页查询
+     *
+     * @return
+     */
+    @RequestMapping(value = "/findPage")
+    @ResponseBody
+    public Page findPage(QueryParam queryParam) {
+        return roleService.findPage(queryParam);
+    }
+
     /**
      * 跳转至角色管理主界面
      *
@@ -22,4 +41,6 @@ public class RoleController {
     public String index() {
         return "/role/index";
     }
+
+
 }
