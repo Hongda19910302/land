d9df902aa206a38f364fcacc48808b59bd75a652
syacht <lisq037@163.com>
2015-10-20 10:24:55 +0800

    填充下拉选择数据

--- src/main/java/net/deniro/land/module/system/service/ModuleService.java
+++ src/main/java/net/deniro/land/module/system/service/ModuleService.java
@@ -2,6 +2,7 @@ package net.deniro.land.module.system.service;
 
 import net.deniro.land.module.system.dao.MenuDao;
 import net.deniro.land.module.system.dao.ModuleSearchCfgDao;
+import net.deniro.land.module.system.entity.Company;
 import net.deniro.land.module.system.entity.MenuItem;
 import net.deniro.land.module.system.entity.ModuleSearchCfg;
 import org.apache.log4j.Logger;
@@ -9,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+
+import static net.deniro.land.module.system.entity.ModuleSearchCfg.InputType;
+import static net.deniro.land.module.system.entity.ModuleSearchCfg.SelectListDataSetType;
 
 /**
  * 模块
@@ -25,6 +31,9 @@ public class ModuleService {
     @Autowired
     private ModuleSearchCfgDao moduleSearchCfgDao;
 
+    @Autowired
+    private CompanyService companyService;
+
     /**
      * 查询
      *
@@ -33,7 +42,25 @@ public class ModuleService {
      */
     public List<ModuleSearchCfg> findByModuleId(Integer moduleId) {
         try {
-            return moduleSearchCfgDao.findByModuleId(moduleId);
+            List<ModuleSearchCfg> cfgs = moduleSearchCfgDao.findByModuleId(moduleId);
+
+
+            for (ModuleSearchCfg cfg : cfgs) {
+                switch (InputType.valueOf(cfg.getInputType())) {
+                    case TEXT:
+                        break;
+                    case SELECT://填充下拉选择数据
+                        switch (SelectListDataSetType.valueOf(cfg
+                                .getSelectListDataSetType())) {
+                            case COMPANY:
+                                cfg.setSelectListDataSet(companyService
+                                        .findAllInSelect());
+                                break;
+                        }
+                        break;
+                }
+            }
+            return cfgs;
         } catch (Exception e) {
             logger.error("查询", e);
             return new ArrayList<ModuleSearchCfg>();
