558dc2e1b93fa2826d10ae9b43cf5c43d2bab8c1
syacht <lisq037@163.com>
2015-10-23 18:31:48 +0800

    表名称转换生成类的名称

--- src/test/java/net/deniro/land/common/utils/code/mysql/AnnotationHibernateCodeMarkerTest.java
+++ src/test/java/net/deniro/land/common/utils/code/mysql/AnnotationHibernateCodeMarkerTest.java
@@ -1,5 +1,6 @@
 package net.deniro.land.common.utils.code.mysql;
 
+import net.deniro.land.common.utils.StrUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,13 +31,21 @@ public class AnnotationHibernateCodeMarkerTest {
 
     @Test
     public void testMark() throws Exception {
+
+        List<String> tableNames = new ArrayList<String>();
+        tableNames.add("comp_page_search");
+        tableNames.add("comp_page_search_form");
+        tableNames.add("comp_page_search_table");
+
         Map<String, String> names = new HashMap<String, String>();
-        names.put("comp_page_search", "CompPageSearch");
-        names.put("comp_page_search_form", "comp_page_search_form");
-        names.put("comp_page_search_table", "comp_page_search_table");
+        for (String tableName : tableNames) {
+            //表名称转换生成类的名称
+            names.put(tableName, StrUtils
+                    .replaceUnderLineAndFirstUpper(tableName, true));
+        }
 
-        String packagePath="net.deniro.land.module.system.entity";
+        String packagePath = "net.deniro.land.module.component.entity";
 
-        codeMarker.mark(packagePath,names);
+        codeMarker.mark(packagePath, names);
     }
 }
