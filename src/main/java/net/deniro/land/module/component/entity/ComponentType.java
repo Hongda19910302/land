fd1d835d20ab5d24a8d487a40ea1d044fc456646
syacht <lisq037@163.com>
2015-10-26 18:01:20 +0800

    组件类型

---
+++ src/main/java/net/deniro/land/module/component/entity/ComponentType.java
@@ -0,0 +1,14 @@
+package net.deniro.land.module.component.entity;
+
+/**
+ * 组件类型
+ *
+ * @author deniro
+ *         2015/10/26
+ */
+public enum ComponentType {
+    /**
+     * 分页查询
+     */
+    PAGE_SEARCH
+}
