770e4187483d055a76d269d5e792a1fe3aed84d6
syacht <lisq037@163.com>
2015-10-26 18:01:30 +0800

    输入框类型

---
+++ src/main/java/net/deniro/land/module/component/entity/InputType.java
@@ -0,0 +1,19 @@
+package net.deniro.land.module.component.entity;
+
+/**
+ * 输入框类型
+ *
+ * @author deniro
+ *         2015/10/26
+ */
+public enum InputType {
+    /**
+     * 文本输入框
+     */
+    TEXT,
+
+    /**
+     * 下拉选择框
+     */
+    SELECT
+}
