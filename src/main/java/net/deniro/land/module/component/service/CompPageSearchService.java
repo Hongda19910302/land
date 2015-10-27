d15a05c22dd1e0f932095895963e73739269cfe9
syacht <lisq037@163.com>
2015-10-27 10:53:38 +0800

    处理表格和查询条件表单的数据转换

--- src/main/java/net/deniro/land/module/component/service/CompPageSearchService.java
+++ src/main/java/net/deniro/land/module/component/service/CompPageSearchService.java
@@ -2,9 +2,20 @@ package net.deniro.land.module.component.service;
 
 import net.deniro.land.module.component.dao.CompPageSearchDao;
 import net.deniro.land.module.component.entity.CompPageSearch;
+import net.deniro.land.module.component.entity.CompPageSearchForm;
+import net.deniro.land.module.component.entity.CompPageSearchTable;
+import net.deniro.land.module.component.entity.InputType;
+import net.deniro.land.module.system.entity.DataSetType;
+import net.deniro.land.module.system.entity.ModuleSearchCfg;
+import net.deniro.land.module.system.service.CompanyService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 分页查询组件
@@ -20,6 +31,12 @@ public class CompPageSearchService {
     @Autowired
     private CompPageSearchDao compPageSearchDao;
 
+    @Autowired
+    private CompanyService companyService;
+
+    @Resource(name = "roleStatus")
+    private Map<String, String> roleStatus;
+
     /**
      * 查询 分页查询组件配置信息
      *
@@ -28,7 +45,49 @@ public class CompPageSearchService {
      */
     public CompPageSearch findById(Integer id) {
         try {
-            return compPageSearchDao.load(id);
+            CompPageSearch pageSearch = compPageSearchDao.load(id);
+
+            /**
+             * 处理表格中的数据转换
+             */
+            Set<CompPageSearchTable> tableFields = pageSearch
+                    .getCompPageSearchTableFields();
+            for (CompPageSearchTable field : tableFields) {
+                String dataSetType = field
+                        .getTransformDataSetType();
+                if (StringUtils.isBlank(dataSetType)) {
+                    continue;
+                }
+                switch (DataSetType.valueOf(dataSetType)) {
+                    case ROLE_STATUS:
+                        field.setTransformDataSet(roleStatus);
+                }
+            }
+            pageSearch.setCompPageSearchTableFields(tableFields);
+
+            /**
+             * 处理查询表单中的数据集转换
+             */
+            Set<CompPageSearchForm> formFields = pageSearch.getCompPageSearchFormFields();
+            for (CompPageSearchForm field : formFields) {
+                switch (InputType.valueOf(field.getInputType())) {
+                    case TEXT:
+                        break;
+                    case SELECT://填充下拉选择数据
+                        switch (DataSetType.valueOf(field
+                                .getSelectListDataSetType())) {
+                            case COMPANY:
+                                field.setSelectListDataSet(companyService
+                                        .findAllInSelect());
+                                break;
+                            case ROLE_STATUS:
+                                field.setSelectListDataSet(roleStatus);
+                        }
+                        break;
+                }
+            }
+
+            return pageSearch;
         } catch (Exception e) {
             logger.error("查询 分页查询组件配置信息", e);
             return new CompPageSearch();
