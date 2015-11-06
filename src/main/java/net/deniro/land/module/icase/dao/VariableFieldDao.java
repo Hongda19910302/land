02b76e0f48b0e555ee79070d55bde20b8ec500ef
syacht <lisq037@163.com>
2015-11-06 11:14:38 +0800

    依据单位ID，获取字段信息（待测试）

--- src/main/java/net/deniro/land/module/icase/dao/VariableFieldDao.java
+++ src/main/java/net/deniro/land/module/icase/dao/VariableFieldDao.java
@@ -7,14 +7,33 @@ import net.deniro.land.module.icase.entity.VariableFieldQueryParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static net.deniro.land.module.icase.entity.TVariableField.BelongToTable.T_CASE;
+import static net.deniro.land.module.icase.entity.TVariableField.Status.*;
+
 /**
- * 案件字段
+ * 字段（主要是案件字段）
  *
  * @author deniro
  *         2015/11/4
  */
 @Repository
 public class VariableFieldDao extends BaseDao<TVariableField> {
+
+    /**
+     * 依据单位ID，获取字段信息
+     *
+     * @param companyId
+     * @return
+     */
+    public List<TVariableField> findByCompanyId(Integer companyId) {
+        StringBuilder hql = new StringBuilder(" from TVariableField t where 1=1 ");
+        hql.append(" and company.companyId = ?");
+        hql.append(" and tableType =  ").append(T_CASE.code());
+        hql.append(" and status =").append(AVAILABLE.code());
+        return this.find(hql.toString(), companyId);
+    }
 
     /**
      * 分页查询
--- src/main/java/net/deniro/land/module/icase/entity/TVariableField.java
+++ src/main/java/net/deniro/land/module/icase/entity/TVariableField.java
@@ -7,12 +7,11 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 /**
-*
- * 案件字段
-*
-* @author deniro（AnnotationHibernateCodeMarker）
-*         2015-11-4
-*/
+ * 字段（主要是案件字段）
+ *
+ * @author deniro（AnnotationHibernateCodeMarker）
+ *         2015-11-4
+ */
 @Data
 @Entity
 @Table(name = "t_variable_field")
@@ -20,76 +19,76 @@ public class TVariableField implements Serializable {
 
 
     /**
-    * 主键ID
-    */
+     * 主键ID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "variable_field_id", nullable = true, length = 10)
     private Integer variableFieldId;
 
     /**
-    * 客户端对应字段key
-    */
+     * 客户端对应字段key
+     */
     @Column(name = "field_key", nullable = true, length = 30)
     private String fieldKey;
 
     /**
-    * 数据库对应表字段ID
-    */
+     * 数据库对应表字段ID
+     */
     @Column(name = "data_field_id", nullable = true, length = 10)
     private Integer dataFieldId;
 
     /**
-    * 单位ID
-    */
+     * 单位ID
+     */
     @Column(name = "company_id", nullable = true, length = 10)
     private Integer companyId;
 
     /**
-    * 字段别名
-    */
+     * 字段别名
+     */
     @Column(name = "alias", nullable = true, length = 30)
     private String alias;
 
     /**
-    * 所属表类型 0：案件表
-    */
+     * 所属表类型 0：案件表
+     */
     @Column(name = "table_type", nullable = true, length = 10)
     private Integer tableType;
 
     /**
-    * 状态 0:可用 1：禁用
-    */
+     * 状态 0:可用 1：禁用
+     */
     @Column(name = "status", nullable = true, length = 10)
     private Integer status;
 
     /**
-    * 字段类型 0：整数 1：带小数 2：字符串 3：日期字符串
-    */
+     * 字段类型 0：整数 1：带小数 2：字符串 3：日期字符串
+     */
     @Column(name = "type", nullable = true, length = 10)
     private Integer type;
 
     /**
-    * 是否必填 0：必填 1：非必填
-    */
+     * 是否必填 0：必填 1：非必填
+     */
     @Column(name = "is_null", nullable = true, length = 10)
     private Integer isNull;
 
     /**
-    * 是否下拉 0：下拉1：非下拉
-    */
+     * 是否下拉 0：下拉1：非下拉
+     */
     @Column(name = "is_pulldown", nullable = true, length = 10)
     private Integer isPulldown;
 
     /**
-    * 是否隐藏 0：隐藏 1：显示
-    */
+     * 是否隐藏 0：隐藏 1：显示
+     */
     @Column(name = "is_hide", nullable = true, length = 10)
     private Integer isHide;
 
     /**
-    * 是否是默认值：1是  0或者空则不是
-    */
+     * 是否是默认值：1是  0或者空则不是
+     */
     @Column(name = "is_default", nullable = true, length = 10)
     private Integer isDefault;
 
@@ -106,6 +105,82 @@ public class TVariableField implements Serializable {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "data_field_id", referencedColumnName = "data_field_id", nullable = true, insertable = false, updatable = false)
     private TDataField dataField;
+
+    /**
+     * 所属表
+     */
+    public enum BelongToTable {
+        /**
+         * 案件表
+         */
+        T_CASE(0);
+
+        private int code;
+
+        BelongToTable(int code) {
+            this.code = code;
+        }
+
+        /**
+         * 获取枚举对象
+         *
+         * @param code 码
+         * @return
+         */
+        public static BelongToTable get(int code) {
+            BelongToTable[] sources = BelongToTable.values();
+            for (BelongToTable source : sources) {
+                if (source.code() == code) {
+                    return source;
+                }
+            }
+            return null;
+        }
+
+        public int code() {
+            return code;
+        }
+    }
+
+    /**
+     * 状态
+     */
+    public enum Status {
+        /**
+         * 可用
+         */
+        AVAILABLE(0),
+        /**
+         * 禁用
+         */
+        disable(1);
+
+        private int code;
+
+        Status(int code) {
+            this.code = code;
+        }
+
+        /**
+         * 获取枚举对象
+         *
+         * @param code 码
+         * @return
+         */
+        public static Status get(int code) {
+            Status[] sources = Status.values();
+            for (Status source : sources) {
+                if (source.code() == code) {
+                    return source;
+                }
+            }
+            return null;
+        }
+
+        public int code() {
+            return code;
+        }
+    }
 
 
 }
