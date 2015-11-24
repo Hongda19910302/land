e43cbcdbbd683cacb622af49d9e63d5e0f20f446
syacht <lisq037@163.com>
2015-11-24 11:01:09 +0800

    查询区域树节点

--- src/main/java/net/deniro/land/module/component/CompController.java
+++ src/main/java/net/deniro/land/module/component/CompController.java
@@ -3,10 +3,13 @@ package net.deniro.land.module.component;
 import net.deniro.land.common.spring.mvc.ResourcePathExposer;
 import net.deniro.land.module.component.entity.CompanyTreeNode;
 import net.deniro.land.module.component.entity.TreeQueryParam;
-import net.deniro.land.module.system.entity.Company;
 import net.deniro.land.module.system.entity.Department;
+import net.deniro.land.module.system.entity.TRegion;
+import net.deniro.land.module.system.entity.User;
 import net.deniro.land.module.system.service.CompanyService;
 import net.deniro.land.module.system.service.DepartmentService;
+import net.deniro.land.module.system.service.RegionService;
+import net.deniro.land.module.system.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +38,49 @@ public class CompController {
 
     @Autowired
     private CompanyService companyService;
+
+    @Autowired
+    private RegionService regionService;
+
+
+    /**
+     * 跳转至区域选择组件
+     *
+     * @param pageSearchComponentId 分页查询组件ID
+     * @return
+     */
+    @RequestMapping(value = "/lookupRegion")
+    public String lookupRegion(Integer pageSearchComponentId, ModelMap mm) {
+        mm.addAttribute("pageSearchComponentId", pageSearchComponentId);
+        return "/component/lookup-region";
+    }
+
+    /**
+     * 查询区域树节点
+     * @param regionId 区域ID
+     * @return
+     */
+    @RequestMapping(value = "/findRegionTreeNode")
+    @ResponseBody
+    public List<TRegion> findRegionTreeNode(String
+            regionId, HttpSession session) {
+
+        List<TRegion> data = new ArrayList<TRegion>();
+
+
+        if (StringUtils.isBlank(regionId)) {//第一次加载，获取当前登录用户归属公司的所在区域
+            User user = (User) session.getAttribute(UserService.USER_CODE);
+            if (user == null) {
+                return data;
+            }
+            data.addAll(regionService.findByCompanyIdForTree(user.getCompanyId()));
+        } else {
+            data.addAll(regionService.findChildrenByRegionIdForTree(NumberUtils.toInt
+                    (regionId)));
+        }
+
+        return data;
+    }
 
     /**
      * 查询所有单位树节点
@@ -89,8 +136,10 @@ public class CompController {
      * @return
      */
     @RequestMapping(value = "/lookupCompanyDepartment")
-    public String lookupCompanyDepartment(Integer pageSearchComponentId,ModelMap mm) {
+    public String lookupCompanyDepartment(Integer pageSearchComponentId, ModelMap mm) {
         mm.addAttribute("pageSearchComponentId", pageSearchComponentId);
         return "/component/lookup-company-department";
     }
+
+
 }
