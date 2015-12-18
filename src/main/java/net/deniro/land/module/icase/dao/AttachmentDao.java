e923e278df8410e76e318a1de0684554694d3bd4
syacht <lisq037@163.com>
2015-12-18 15:37:41 +0800

    依据关联ID和类型，删除所有相关附件记录

--- src/main/java/net/deniro/land/module/icase/dao/AttachmentDao.java
+++ src/main/java/net/deniro/land/module/icase/dao/AttachmentDao.java
@@ -2,10 +2,14 @@ package net.deniro.land.module.icase.dao;
 
 import net.deniro.land.common.dao.BaseDao;
 import net.deniro.land.module.icase.entity.TAttachment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static net.deniro.land.module.icase.entity.TAttachmentRelation.RelationType;
 
@@ -17,6 +21,34 @@ import static net.deniro.land.module.icase.entity.TAttachmentRelation.RelationTy
  */
 @Repository
 public class AttachmentDao extends BaseDao<TAttachment> {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    /**
+     * 删除所有相关记录
+     *
+     * @param relationId   关联的ID
+     * @param relationType 关联类型
+     * @return 返回受影响的条数
+     */
+    public int deleteAll(Integer relationId, RelationType relationType) {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("relationId", relationId);
+        params.put("relationType", relationType.code());
+
+        String delRelationSql = "DELETE FROM " +
+                "t_attachment_relation WHERE " +
+                "RELATION_ID=:relationId AND RELATION_TYPE=:relationType";
+
+        String delAttachmentSql = "DELETE FROM t_attachment  WHERE ATTACHMENT_ID in(SELECT " +
+                "w.ATTACHMENT_ID FROM t_attachment_relation w WHERE w.RELATION_ID=:relationId AND w.RELATION_TYPE=:relationType)";
+
+        int count = 0;
+        count = count + namedParameterJdbcTemplate.update(delRelationSql, params);
+        count = count + namedParameterJdbcTemplate.update(delAttachmentSql, params);
+        return count;
+    }
 
     /**
      * 查询附件列表
--- src/test/java/net/deniro/land/module/icase/dao/AttachmentDaoTest.java
+++ src/test/java/net/deniro/land/module/icase/dao/AttachmentDaoTest.java
@@ -1,5 +1,6 @@
 package net.deniro.land.module.icase.dao;
 
+import net.deniro.land.module.icase.entity.TAttachmentRelation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import static net.deniro.land.module.icase.entity.TAttachmentRelation.RelationType.AUDIT;
 import static net.deniro.land.module.icase.entity.TAttachmentRelation.RelationType.CASE;
 
 /**
@@ -18,13 +18,20 @@ import static net.deniro.land.module.icase.entity.TAttachmentRelation.RelationTy
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/spring-context-base.xml",
         "classpath:spring/spring-context-db.xml", "classpath:spring/spring-context-tx" +
-        ".xml", "classpath:spring/spring-context-dataset-type.xml"})
+        ".xml", "classpath:spring/spring-context-dataset-type.xml",
+        "classpath:spring/spring-context-ftp.xml"})
 @TransactionConfiguration
 @Transactional
 public class AttachmentDaoTest {
 
     @Autowired
     private AttachmentDao attachmentDao;
+
+    @Test
+    public void deleteAll() {
+        System.out.printf("$$$$$$$$$$$$$$" + attachmentDao.deleteAll(559,
+                TAttachmentRelation.RelationType.CASE));
+    }
 
     @Test
     public void testFindByAuditIdAndType() throws Exception {
