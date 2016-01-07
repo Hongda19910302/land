34000a8fb77f5bd83d6fa45bb081d90a480a39f8
syacht <lisq037@163.com>
2016-01-07 11:09:19 +0800

    依据单位ID，获取与区域的映射关系

--- src/main/java/net/deniro/land/module/system/dao/RegionRelationDao.java
+++ src/main/java/net/deniro/land/module/system/dao/RegionRelationDao.java
@@ -4,6 +4,8 @@ import net.deniro.land.common.dao.BaseDao;
 import net.deniro.land.module.system.entity.TRegionRelation;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * 单位或部门与区域映射关系
  *
@@ -12,4 +14,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class RegionRelationDao extends BaseDao<TRegionRelation> {
+
+    /**
+     * 依据单位ID，获取与区域的映射关系
+     *
+     * @param companyId
+     * @return
+     */
+    public List<TRegionRelation> findByCompanyId(Integer companyId) {
+        String hql = "from TRegionRelation t where t.relationType=0 and t" +
+                ".relationId=?";
+        return this.find(hql, companyId);
+    }
 }
---
+++ src/test/java/net/deniro/land/module/system/dao/RegionRelationDaoTest.java
@@ -0,0 +1,31 @@
+package net.deniro.land.module.system.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author deniro
+ *         2016/1/7
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/spring-context-base.xml",
+        "classpath:spring/spring-context-db.xml", "classpath:spring/spring-context-tx" +
+        ".xml", "classpath:spring/spring-context-dataset-type.xml",
+        "classpath:spring/spring-context-ftp.xml"})
+@TransactionConfiguration
+@Transactional
+public class RegionRelationDaoTest {
+
+    @Autowired
+    private RegionRelationDao regionRelationDao;
+
+    @Test
+    public void testFindByCompanyId() throws Exception {
+        System.out.println("$$$$$$" + regionRelationDao.findByCompanyId(1));
+    }
+}
