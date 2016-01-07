42fa1272914c2af0baf1780e9413ec342fd8d8bc
syacht <lisq037@163.com>
2016-01-07 14:17:34 +0800

    依据单位ID，删除所有与此单位相关的区域映射关系

--- src/main/java/net/deniro/land/module/system/dao/RegionRelationDao.java
+++ src/main/java/net/deniro/land/module/system/dao/RegionRelationDao.java
@@ -2,9 +2,13 @@ package net.deniro.land.module.system.dao;
 
 import net.deniro.land.common.dao.BaseDao;
 import net.deniro.land.module.system.entity.TRegionRelation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 单位或部门与区域映射关系
@@ -14,6 +18,24 @@ import java.util.List;
  */
 @Repository
 public class RegionRelationDao extends BaseDao<TRegionRelation> {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    /**
+     * 依据单位ID，删除所有与此单位相关的区域映射关系
+     *
+     * @param companyId
+     * @return
+     */
+    public int deleteAllByCompanyId(Integer companyId) {
+        String sql = "delete from t_region_relation where relation_type=0 and relation_id=:companyId";
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("companyId", companyId);
+
+        return namedParameterJdbcTemplate.update(sql, params);
+    }
 
     /**
      * 依据单位ID，获取与区域的映射关系
